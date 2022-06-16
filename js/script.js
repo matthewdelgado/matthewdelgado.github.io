b24ea3db9890bc1974f87b8b37fe57b3e40bccdb
Matthew Delgado <64821032+matthewdelgado@users.noreply.github.com>
2022-06-16 14:06:10 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 /* ============================== typing animation ============================ */
-var typed = new Typed(".typing",{
+var typed = new Typed(".typing", {
     strings:["Software Engineer","Student","Dreamer"],
     typeSpeed:80,
     BackSpeed:50,
-    loop:true;
+    loop:true,
 })
 /* ============================== Aside ============================ */
 const nav = document.querySelector(".nav"),
